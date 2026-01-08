7748726479461a453fc7f7df402d72bd0f8279c5
Jullian Russell Gerona <j.r.gerona2007@gmail.com>
2026-01-08 22:13:28 +0800

    Add files via upload
    Pallindrome checker for arrays

---
+++ Arrays4.java
@@ -0,0 +1,22 @@
+
+public class Arrays4 {
+public static void main (String[] args) {
+   //Return true if the array reads the same forwards and backwards.
+   int [] arr = {6,7,7,6};
+   int z = arr.length - 1;
+   boolean n = true;
+   for (int x = 0; x<arr.length/2; x++){
+    if (arr[x] != arr[z]){
+        n = false;
+        break;
+    }
+    z--;
+
+   } if (n){
+    System.out.print(n);
+   } else {
+    System.out.print(n);
+   }
+} 
+}
+
